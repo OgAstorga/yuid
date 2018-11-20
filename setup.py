27688d8207f2249a8e6585dc7c0b5db989121a2a
Og Astorga <og@ogastorga.com>
2018-11-20 14:59:49 -0600

    change order of things

--- setup.py
+++ setup.py
@@ -14,34 +14,26 @@ with open(path.join(here, 'yuid', 'version.txt')) as f:
 
 setup(
     name='yuid',
-    description='youtube-like UUIDs',
-    long_description=long_description,
-    url='https://github.com/ogastorga/yuid',
-
     version=version,
-
     author='Og Astorga',
     author_email='og@ogastorga.com',
-    license='MIT',
+    description='youtube-like UUIDs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ogastorga/yuid',
+
+    packages=[
+        'yuid',
+    ],
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
     ],
 
     keywords=[
         'uuid',
-    ],
-
-    packages=[
-        'yuid',
     ],
 
     test_require=[
